fa4de11795f1311035b304bb35737c0448daf40e
Annukprince <annukprince@gmail.com>
2024-08-16 14:17:53 +0530

    contact manager

--- src/main/java/com/ust/ContactRepository.java
+++ src/main/java/com/ust/ContactRepository.java
@@ -2,29 +2,51 @@ package com.ust;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class ContactRepository {
 
-    private List<Contact> contacts = new LinkedList();
+    private List<Contact> contacts = new LinkedList<>();
 
-    private long generateId() {
-        // TODO
+    public long generateId() {
+        long id = contacts.stream().map(Contact::getId).max(Long::compare).orElse(0L);
+        return id + 1;
     }
 
-    public Contact save(Contact contact) {
-        // TODO
+    public Contact save(String name, String phoneNumber) {
+        Optional<Contact> existingContactOpt = contacts.stream()
+                .filter(c -> c.getName().equals(name))
+                .findFirst();
+        if (existingContactOpt.isPresent()) {
+            var existingContact = existingContactOpt.get();
+            existingContact.setPhoneNumber(phoneNumber);
+            return existingContact;
+        }
+        Contact contact = new Contact(generateId(), name, phoneNumber);
+        contacts.add(contact);
+        return contact;
     }
 
-    public Contact findById(String id) throws ContactNotFoundException {
-        // TODO
+    public Contact findById(long id) throws ContactNotFoundException {
+        return contacts.stream()
+                .filter(c -> c.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new ContactNotFoundException("Contact with id " + id + " not found"));
     }
 
-    public List<Contact> findByPhoneNumber(String phoneNumber) throws ContactNotFoundException {
-        // TODO
+    public Contact findByPhoneNumber(String phoneNumber) throws ContactNotFoundException {
+        return contacts.stream()
+                .filter(c -> c.getPhoneNumbers().contains(phoneNumber))
+                .findFirst()
+                .orElseThrow(() -> new ContactNotFoundException("Contact not found"));
     }
 
-    public List<Contact> findByName(String name) throws ContactNotFoundException {
-        // TODO
+    public Contact findByName(String name) throws ContactNotFoundException {
+        return contacts.stream()
+                .filter(c -> c.getName().equals(name))
+                .findFirst()
+                .orElseThrow(() -> new ContactNotFoundException("Contact not found"));
     }
 
 }
